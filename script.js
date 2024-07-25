a54d4b58110bb11d3de977a9b5f810cd954e9ce8
Gihan_Sachith <gihansachith92@gmail.com>
2024-07-25 18:38:44 +0530

    progress bar percentage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -87,6 +89,17 @@ function nextSong(){
 // onload select first song
 loadSong(songs[songIndex]);
 
+// updateProgressBar and time
+function updateProgressBar(e){
+  if(isPlaying){
+    const {duration , currentTime} = e.srcElement;
+    // upgrade progress bar width
+    const progressPercent = (currentTime/duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 // event listners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate',updateProgressBar);
